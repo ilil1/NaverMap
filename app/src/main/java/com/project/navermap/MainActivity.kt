a09dfd5f5b7e36142f2342370e4385dd10e56a71
k <free.rjs102@gmail.com>
2022-06-28 18:21:24 +0900

    6.28 현재 위치 출력

--- app/src/main/java/com/project/navermap/MainActivity.kt
+++ app/src/main/java/com/project/navermap/MainActivity.kt
@@ -1,17 +1,26 @@
 package com.project.navermap
 
+import android.Manifest
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
-import androidx.fragment.app.FragmentManager
-import com.naver.maps.map.MapFragment
-import com.naver.maps.map.NaverMap
-import com.naver.maps.map.OnMapReadyCallback
+import androidx.core.app.ActivityCompat
+import com.naver.maps.map.*
+import com.naver.maps.map.util.FusedLocationSource
 
 class MainActivity : AppCompatActivity() , OnMapReadyCallback {
 
     private lateinit var naverMap: NaverMap
+    private lateinit var locationSource: FusedLocationSource
+
+    companion object {
+        private const val LOCATION_PERMISSION_REQUEST_CODE = 1000
+
+        private val PERMISSIONS = arrayOf(
+            Manifest.permission.ACCESS_FINE_LOCATION,
+            Manifest.permission.ACCESS_COARSE_LOCATION)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,10 +28,35 @@ class MainActivity : AppCompatActivity() , OnMapReadyCallback {
 
         var mapFragment: MapFragment = supportFragmentManager.findFragmentById(R.id.map_fragment) as MapFragment
         mapFragment.getMapAsync(this)
+
+        locationSource = FusedLocationSource(this, LOCATION_PERMISSION_REQUEST_CODE)
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray)
+    {
+        Log.d("onRequest", "onRequestPermissionsResult")
+
+        if (locationSource.onRequestPermissionsResult(requestCode, permissions, grantResults)) {
+            if (!locationSource.isActivated) { // 권한 거부됨
+                Log.d("권한 거부", "권한 거부됨")
+                naverMap.locationTrackingMode = LocationTrackingMode.None
+            }
+            else {
+                Log.d("권한 승인", "권한 승인됨")
+                naverMap.locationTrackingMode = LocationTrackingMode.Follow // 현위치 버튼 컨트롤 활성
+            }
+            return
+        }
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
     }
 
     override fun onMapReady(map : NaverMap) {
-        naverMap = map
+        this.naverMap = map
+
+        naverMap.uiSettings.isLocationButtonEnabled = true
+        naverMap.locationSource = locationSource
+
+        ActivityCompat.requestPermissions(this, PERMISSIONS, LOCATION_PERMISSION_REQUEST_CODE)
         Toast.makeText(this, "맵 초기화 완료", Toast.LENGTH_LONG).show()
     }
 }
