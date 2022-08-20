fa865406c94116c98d0f37170d7beffa8210fe13
k <free.rjs102@gmail.com>
2022-08-20 12:59:30 +0900

    update

--- app/src/main/java/com/project/navermap/widget/adapter/ModelRecyclerAdapter.kt
+++ app/src/main/java/com/project/navermap/widget/adapter/ModelRecyclerAdapter.kt
@@ -1,7 +1,9 @@
 package com.project.navermap.widget.adapter
 
+import android.annotation.SuppressLint
 import android.view.ViewGroup
 import androidx.lifecycle.ViewModel
+import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import com.project.navermap.MapApplication
 import com.project.navermap.domain.model.CellType
@@ -19,7 +21,7 @@ open class ModelRecyclerAdapter<M : Model, VM : ViewModel> (
     private val viewModel: VM,
     private val resourcesProvider: ResourcesProvider,
     private val adapterListener: AdapterListener
-) : ListAdapter<Model, ModelViewHolder<M>>(Model.DIFF_CALLBACK) {
+) : ListAdapter<Model, ModelViewHolder<M>>(DIFF_CALLBACK) {
 
     override fun getItemViewType(position: Int): Int = modelList[position].type.ordinal
 
@@ -37,4 +39,41 @@ open class ModelRecyclerAdapter<M : Model, VM : ViewModel> (
         list?.let { modelList = it }
         super.submitList(list)
     }
+
+
+    companion object {
+        val DIFF_CALLBACK = object : DiffUtil.ItemCallback<Model>() {
+            override fun areItemsTheSame(oldItem: Model, newItem: Model): Boolean {
+                return oldItem.isTheSame(newItem)
+            }
+            @SuppressLint("DiffUtilEquals")
+            override fun areContentsTheSame(oldItem: Model, newItem: Model): Boolean {
+                return oldItem == newItem
+            }
+        }
+    }
 }
+
+///**
+// * [DIFF_CALLBACK]의 areContentsTheSame에서 사용할 equals
+// * equals가 없으면 areContentsTheSame에 @SuppressLint("DiffUtilEquals")를 붙여야한다.
+// * Model 클래스를 비교할 일이 없으므로 Annotation을 붙여도 상관없다.
+// */
+//override fun equals(other: Any?): Boolean {
+//    if (this === other) return true
+//    if (javaClass != other?.javaClass) return false
+//
+//    other as Model
+//
+//    if (id != other.id) return false
+//    if (type != other.type) return false
+//
+//    return true
+//}
+//
+//override fun hashCode(): Int {
+//    var result = id.hashCode()
+//    result = 31 * result + type.hashCode()
+//    return result
+//}
+
