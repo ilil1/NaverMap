56439b8fd58de0bb98ea3da1e6fb5bae3f5650be
HEETAE HEO <97378151+heetaeheo@users.noreply.github.com>
2022-08-23 14:57:11 +0900

    infinite ViewPager

--- app/src/main/java/com/project/navermap/widget/adapter/SliderAdapter.kt
+++ app/src/main/java/com/project/navermap/widget/adapter/SliderAdapter.kt
@@ -23,7 +23,7 @@ class SliderAdapter(
     inner class SliderViewHolder(val binding: SlideItemContainerBinding) :
         RecyclerView.ViewHolder(binding.root) {
         fun bind(Item: SliderItemModel) {
-            binding.itemNameText.text = position.toString()
+            binding.itemNameText.text = (position % 3 + 1).toString()
             binding.pagerimage.setImageResource(Item.image)
         }
     }
@@ -32,13 +32,15 @@ class SliderAdapter(
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SliderViewHolder {
         val binding =
-            SlideItemContainerBinding.inflate(LayoutInflater.from(parent.context),
-                parent, false)
+            SlideItemContainerBinding.inflate(
+                LayoutInflater.from(parent.context),
+                parent, false
+            )
         return SliderViewHolder(binding)
     }
 
     override fun onBindViewHolder(holder: SliderViewHolder, position: Int) {
-        holder.bind(sliderItems[position])
+        holder.bind(sliderItems[(position % 3)])
         Log.d("sliderItems", sliderItems.size.toString())
         Log.d("position", position.toString())
         //position이 3일때 다시 post 한다.
