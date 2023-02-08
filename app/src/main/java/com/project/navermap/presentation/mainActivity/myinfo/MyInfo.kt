bbb0eeb309a1c8eac219a1311d22bb16b381b17a
heoheetae <htheo@autocrypt.io>
2023-02-09 00:02:50 +0900

    Compose Activity create

---
+++ app/src/main/java/com/project/navermap/presentation/mainActivity/myinfo/MyInfo.kt
@@ -0,0 +1,12 @@
+package com.project.navermap.presentation.mainActivity.myinfo
+
+import androidx.compose.runtime.Composable
+import java.lang.reflect.Modifier
+
+@Composable
+fun MyInfo(
+    modifier : Modifier,
+    viewModel : MyInfoViewModel,
+){
+
+}
