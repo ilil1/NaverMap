6432f54e40deab41fcb8485eac5998aaa339acdb
heoheetae <htheo@autocrypt.io>
2023-02-13 23:26:18 +0900

    Compose gradle 업데이트 중 kaptDebugKotlin 에러 발생 내일 잡을 예정

--- app/src/main/java/com/project/navermap/presentation/base/BaseFragment.kt
+++ app/src/main/java/com/project/navermap/presentation/base/BaseFragment.kt
@@ -1,11 +1,13 @@
 package com.project.navermap.presentation.base
 
+import android.app.Dialog
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.viewbinding.ViewBinding
+import com.project.navermap.R
 import com.project.navermap.util.provider.ResourcesProvider
 import kotlinx.coroutines.Job
 
@@ -14,7 +16,17 @@ abstract class BaseFragment<VB: ViewBinding>: Fragment() {
     private var _binding: VB? = null
     protected val binding : VB get() = _binding!!
 
+    lateinit var progressDialog: Dialog
+
     abstract fun getViewBinding(): VB
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        progressDialog = Dialog(requireContext())
+        progressDialog.setContentView(R.layout.dialog_progress)
+        progressDialog.window?.setBackgroundDrawableResource(android.R.color.transparent)
+        progressDialog.setCancelable(false)
+    }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         _binding = getViewBinding()
