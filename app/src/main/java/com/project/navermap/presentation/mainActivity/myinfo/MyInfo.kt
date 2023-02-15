8d0486c1a4f584f0c09b00bec1cbf0a8ae05a91f
heoheetae <htheo@autocrypt.io>
2023-02-15 23:19:05 +0900

    MyInfoActivity -> MyInfoComposeActivity.kt로 변경 및 TopBar 작업

--- app/src/main/java/com/project/navermap/presentation/mainActivity/myinfo/MyInfo.kt
+++ app/src/main/java/com/project/navermap/presentation/mainActivity/myinfo/MyInfo.kt
@@ -1,12 +1,76 @@
 package com.project.navermap.presentation.mainActivity.myinfo
 
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import java.lang.reflect.Modifier
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import com.project.navermap.R
+import com.project.navermap.presentation.ui.extensions.dpToSp
+import com.project.navermap.presentation.ui.theme.ColorBase
+
 
 @Composable
 fun MyInfo(
-    modifier : Modifier,
-    viewModel : MyInfoViewModel,
-){
+    modifier: Modifier = Modifier,
+    viewModel: MyInfoViewModel,
+) {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+    ) {
+        Column(
+            modifier = modifier
+                .fillMaxWidth()
+        ) {
+            topBar(
+                modifier = Modifier
+            )
+        }
+    }
+}
+
+
+@Composable
+private fun topBar(
+    modifier: Modifier = Modifier,
+) {
+    val screenWidth = LocalDensity.current.run { LocalConfiguration.current.screenWidthDp.dp }
+
+    Row(
+        modifier = modifier
+            .fillMaxWidth()
+            .wrapContentHeight()
+            .background(ColorBase),
+        verticalAlignment = Alignment.CenterVertically,
+    ) {
+
+        Image(
+            painter = painterResource(id = R.drawable.left_white),
+            contentDescription = "back",
+            modifier = Modifier
+                .padding(start = 15.dp)
+                .size(20.dp)
+        )
+
+        Text(
+            text = "내 정보",
+            modifier = Modifier
+                .align(Alignment.CenterVertically)
+                .padding(vertical = 12.dp)
+                .padding(start = ( (screenWidth/2) - 60.dp) ),
+            textAlign = TextAlign.Center,
+            fontSize = 20.dpToSp(),
+            color = Color.White
+        )
+    }
 
 }
--- app/src/main/java/com/project/navermap/presentation/mainActivity/myinfo/MyInfoComposeActivity.kt
+++ app/src/main/java/com/project/navermap/presentation/mainActivity/myinfo/MyInfoComposeActivity.kt
@@ -3,23 +3,23 @@ package com.project.navermap.presentation.mainActivity.myinfo
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 
 class MyInfoComposeActivity : ComponentActivity() {
 
+    private val viewModel: MyInfoViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-         setContent {
-            Box(
-                contentAlignment = Alignment.Center,
-                modifier = Modifier.fillMaxSize()
-            ){
+        setContent {
 
-            }
+            MyInfo(
+                modifier = Modifier.fillMaxSize(),
+                viewModel = viewModel
+            )
         }
     }
-
 }
