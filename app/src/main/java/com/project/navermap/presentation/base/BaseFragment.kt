be2252413c42b59cbe40b73280240f628549b48e
쓰리원 <90826931+ilil1@users.noreply.github.com>
2023-02-17 22:58:17 +0900

    [추가] BaseFragment 에 fun observeState() 를 통한 공통 state 관리

--- app/src/main/java/com/project/navermap/presentation/base/BaseFragment.kt
+++ app/src/main/java/com/project/navermap/presentation/base/BaseFragment.kt
@@ -6,10 +6,13 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.lifecycleScope
 import androidx.viewbinding.ViewBinding
 import com.project.navermap.R
 import com.project.navermap.util.provider.ResourcesProvider
 import kotlinx.coroutines.Job
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.launch
 
 abstract class BaseFragment<VB: ViewBinding>: Fragment() {
 
@@ -17,6 +20,8 @@ abstract class BaseFragment<VB: ViewBinding>: Fragment() {
     protected val binding : VB get() = _binding!!
 
     lateinit var progressDialog: Dialog
+
+    var uiState: MutableStateFlow<UiState<Any>> = MutableStateFlow(UiState.Uninitialized)
 
     abstract fun getViewBinding(): VB
 
@@ -50,4 +55,22 @@ abstract class BaseFragment<VB: ViewBinding>: Fragment() {
     open fun initViews() = Unit
 
     open fun observeData() = Unit
+
+    fun observeState() {
+        viewLifecycleOwner.lifecycleScope.launch {
+            uiState.collect {
+                when (it) {
+                    is UiState.Loading -> {
+                        if(it.data) {
+                            progressDialog.show()
+                        } else {
+                            progressDialog.dismiss()
+                        }
+                    }
+                    is UiState.Success -> {}
+                    is UiState.Fail -> {}
+                }
+            }
+        }
+    }
 }
