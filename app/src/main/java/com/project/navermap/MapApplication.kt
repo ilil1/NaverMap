7dad6066c85267b7cc0ac7aad3dc5281adad67ff
heoheetae <htheo@autocrypt.io>
2023-02-19 20:46:43 +0900

    Image SharedPreferences에 저장하는 로직 생성 중

--- app/src/main/java/com/project/navermap/MapApplication.kt
+++ app/src/main/java/com/project/navermap/MapApplication.kt
@@ -3,17 +3,28 @@ package com.project.navermap
 import android.app.Application
 import android.content.Context
 import com.kakao.sdk.common.KakaoSdk
+import com.project.navermap.app.config.AppConfig
 import dagger.hilt.android.HiltAndroidApp
 import javax.inject.Inject
 
 @HiltAndroidApp
 class MapApplication : Application(){
 
+    companion object{
+        lateinit var context: Context
+        lateinit var appConfig: AppConfig
+    }
+
+    init {
+        context = this
+    }
+
     override fun onCreate() {
         super.onCreate()
 
         KakaoSdk.init(this,getString(R.string.kakao_app_key))
 
+        appConfig = AppConfig(context)
     }
 
 }
