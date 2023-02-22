6660505df709568931ea1a490138e2774d726eee
쓰리원 <90826931+ilil1@users.noreply.github.com>
2023-02-22 23:01:43 +0900

    [추가] RestaurantItemCacheDao 메서드 추가

--- app/src/main/java/com/project/navermap/data/db/dao/RestaurantItemCacheDao.kt
+++ app/src/main/java/com/project/navermap/data/db/dao/RestaurantItemCacheDao.kt
@@ -1,4 +1,16 @@
 package com.project.navermap.data.db.dao
 
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.project.navermap.data.entity.restaurant.RestaurantFoodEntity
+
 interface RestaurantItemCacheDao {
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertSearchResults(entities: List<RestaurantFoodEntity>)
+
+    suspend fun getSearchResultCaches(): List<RestaurantFoodEntity>
+
+    suspend fun clearSearchResultCaches()
 }
